5074e43aea0ffabc10ff2f94bda7484dbcb5e097
Oleksii Sheludchenko <alek_shel@icloud.com>
2024-01-15 21:32:43 +0600

    IS_HEROKU_APP DEBUG variable

--- carshop/settings.py
+++ carshop/settings.py
@@ -38,7 +38,9 @@ SECRET_KEY = os.environ.get(
 IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU_APP:
+if IS_HEROKU_APP:
+    DEBUG = os.environ.get("DJANGO_DEBUG", default=True)
+else:
     DEBUG = True
 
 # On Heroku, it's safe to use a wildcard for `ALLOWED_HOSTS``, since the Heroku router performs
